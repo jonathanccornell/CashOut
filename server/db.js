a45470e639cd514bc16533da52515b7c6eda89d2
Jonathan Cornell <jonathancornell@Jonathans-MacBook-Pro-2.local>
2026-04-04 17:02:58 -0400

    Use persistent /data volume for SQLite in production

--- server/db.js
+++ server/db.js
@@ -1,7 +1,10 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, '..', 'cashout.db'));
+const dbPath = process.env.NODE_ENV === 'production'
+  ? '/data/cashout.db'
+  : path.join(__dirname, '..', 'cashout.db');
+const db = new Database(dbPath);
 
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
